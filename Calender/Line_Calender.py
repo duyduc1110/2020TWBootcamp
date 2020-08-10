a794e8b65faef9e7634c4904c103c3a9b3b70b7a
Ubuntu <ubuntu@ip-172-31-44-163.us-west-2.compute.internal>
2020-08-10 06:55:41 +0000

    update code

--- Calender/Line_Calender.py
+++ Calender/Line_Calender.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 from datetime import datetime, timedelta
+from dateutil.parser import parse
+from word2number import w2n
+import re
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -46,29 +49,124 @@ def Build_Google_Service():
     return service, creds_id
 
 
-def Get_StarT_End_Time(Day):
-    start_time = datetime(Day[0], Day[1], Day[2], 10, 0, 0)#.strftime("%Y-%m-%dT%H:%M:%S")
+def time_handler(Time):
+    Time = Time.strip()
+    non_exist_word = [None, '/', '', ' ']  
+    date_word = 'January,February,March,April,May,June,July,August,September,October,November,December'.lower().split(',')
+    # error time
+    for t in non_exist_word:
+        for d in date_word:
+            if t == Time :
+                return '10am'
+            if d in Time:
+                return '10am'
+
+    # Normal one
+    if 'pm' in Time or 'am' in Time:
+        return Time
+
+    # extract correct time
+    Time_ = Time.split()
+    key_word = ["o'clock"]
+    Prepositions = ['on', 'in', 'at']
+    return_time = []
+    for t in Time_:
+        if t in Prepositions:
+            return_time.append(t)
+        else:
+            try:
+                return_time.append(str(w2n.word_to_num(t)))
+                return ''.join(return_time) +':00'
+            except:
+                return '10am'
+
+
+
+
+def Get_Start_End_Time(Date, Time):
+    Base_time = Date[0]
+    Time = time_handler(Time)
+    # split the confusing word
+    key_word_change = {'next':7}
+    with_key = False
+    for key_change in key_word_change.keys():
+        if key_change in Base_time.lower():
+            Base_time = ''.join(re.split(key_change, Base_time))
+            with_key = True
+            break
+
+    # Specific time
+    key_word = {'weekend':'Saturday'}
+    key_word_to_remove = ['this', 'that']
+    for key in key_word.keys():
+        for rm_key in key_word_to_remove:
+            if key in Base_time.lower():
+                Base_time = ''.join(re.split(key, Base_time)) + key_word[key]
+
+            Base_time = ''.join(re.split(rm_key, Base_time))
+            break
+
+    if with_key:
+        start_time = parse(Base_time + ' ' + Time) + timedelta(days = key_word_change[key_change])
+    else:
+        start_time = parse(Base_time + ' ' + Time)
+    
+    # Change time if needed
+    if Date[1] is None or Date[2] is None:
+        day_unit = {'day': 1, 'week' : 7}
+
+        delay = True if Date[1] else False
+        early = True if Date[2] else False
+
+        if delay:
+            for key in day_unit:
+                if key in Date[1][1].lower():
+                  start_time += timedelta(days = Date[1][0] * day_unit[key])
+                  break
+
+        elif early:
+            for key in day_unit:
+                if key in Date[2][1].lower():
+                    start_time -= timedelta(days = Date[2][0] * day_unit[key])
+                    break
+
+    # expect every event last for 4 hours
     end_time = start_time + timedelta(hours = 4)
     
     return start_time.strftime("%Y-%m-%dT%H:%M:%S"), end_time.strftime("%Y-%m-%dT%H:%M:%S")
     
 
-def Create_event (Event = 100, Location = 0, Day = [2020, 7, 18], timeZone = 'Asia/Brunei'):
+def Create_event (Event, timeZone = 'Asia/Brunei'):
+    '''
+      Event : dict
+      Event['Activity'] = 'study'
+      Event['Day'] = tuple(('on this friday', (3,'days'), None ))
+      Event['Time'] = '  '
+      Event['Place'] = 'Elephant Mountain'
+    '''
     service, creds_id = Build_Google_Service()
     events_result = service.events().list(calendarId = creds_id).execute()
     
     # Get Day, Default start from 10 am, end at 2 pm
-    start_time, end_time = Get_StarT_End_Time(Day)
+    start_time, end_time = Get_Start_End_Time(Event['Day'], Event['Time'])
+
+    print(start_time)
+
+    if Event['Activity'] is None:
+        Event['Activity'] = 'Unkonwn'
+
+    if Event['Place'] is None:
+        Event['Place'] = 'Unkonwn'
 
     event = {
-      'summary': Event,
-      'location': Location,
+      'summary': Event['Activity'],
+      'location': Event['Place'],
       'start': {
-        'dateTime': '2020-07-28T09:00:00-07:00',
+        'dateTime': start_time,
         'timeZone': timeZone,
       },
       'end': {
-        'dateTime': '2020-07-28T17:00:00-07:00',
+        'dateTime': end_time,
         'timeZone': timeZone,
       },
       'reminders': {
@@ -86,7 +184,10 @@ def Create_event (Event = 100, Location = 0, Day = [2020, 7, 18], timeZone = 'As
     
 
 if __name__ == '__main__':
-    Event = 'Cinnamon_AI'
-    Location = 'Elephant Mountain'
-    Day = [2020, 7, 28]
-    Create_event(Event, Location, Day)
+    Event = dict()
+    Event['Activity'] = 'study'
+    Event['Day'] = tuple(('on this friday', (3,'days'), None ))
+    Event['Time'] = '  '
+    Event['Place'] = 'Elephant Mountain'
+
+    Create_event(Event)
